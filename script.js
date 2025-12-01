d40a4227d86ebd3aa7e58bee6c47e449b05c67e9
Darksoft-D <nurzhakyp707@gmail.com>
2025-12-02 02:23:44 +0500

    Board Game Club Home Page Finished

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const coin = document.querySelector('.coin');
 const block = document.querySelector('.blockimg');
+const topnav = document.querySelector('.topnav');
 let position = 0;
+var prevScrollpos = window.pageYOffset;
 
-function animateCoin() {
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    topnav.style.top = "0";
+  } else {
+    topnav.style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    root: null, // relative to the viewport
+    rootMargin: '0px',
+    threshold: 0.5 // Trigger when 50% of the element is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // Stop observing once animated
+      }
+    });
+  }, observerOptions);
+
+  const slideInElements = document.querySelectorAll('.slide-in-left, .slide-in-right');
+  slideInElements.forEach(el => {
+    observer.observe(el);
+  });
+});
+
+ function animateCoin() {
     position += 50;
     if (position > 200) {
         position = 0;
@@ -15,4 +49,4 @@ block.addEventListener('click', function handler() {
     coin.classList.add('up');
     setInterval(animateCoin, 100);
     block.removeEventListener('click', handler);
-});
+}); 
